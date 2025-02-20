da4b3a09701d1124244a635bed36a0bbfdbccd6a
3334gFIOP-school <jackson.hauley@ucas-edu.net>
2025-02-20 13:45:09 -0700

    eeee

--- high_score_tracker/main.py
+++ high_score_tracker/main.py
@@ -30,43 +30,38 @@ height = screen.get_height()
 smallfont = pygame.font.SysFont('Arial',35) 
 bigfont = pygame.font.SysFont('Arial',70) 
 
-# Rendering a text written in the font
-quit_button_text = smallfont.render('Quit' , True , white) 
-reaction_button_text = smallfont.render('Reaction' , True , white) 
-
 # COMPLETELY EDITABLE button location variables
 
 quit_button = {
-"width" : 140,
-"height" : 40,
-"StartPos": {"x" : 400 ,"y" : 360}, # Top left coords
+"width" : 140, # width of the button
+"height" : 40, # height of the button
+"StartPos": {"x" : 400 ,"y" : 360}, # Top left is 0,0
 "text": "Quit",
 "font": "Arial",
-"fontsize": 35
+"fontsize": 35,
+"hover_color": color_light,
+"main_color": color_dark
 }
 
 reaction_button = {
-"width" : 140,
-"height" : 40,
-"StartPos": {"x" :  180,"y" : 360}, # Top left coords
-"text": "Reaction",
+"width" : 140, # width of the button
+"height" : 40, # height of the button
+"StartPos": {"x" :  180,"y" : 360}, # Top left is 0,0
+"text": "Reaction", 
 "font": "Arial",
-"fontsize": 35
+"fontsize": 35,
+"hover_color": color_light,
+"main_color": color_dark
 }
 
 # END BUTTONS ==================================
 
-def quit_check(ev):
-    if ev.type == pygame.QUIT: 
-            pygame.quit() 
-            running = False
-
 def button(dict):
     if dict['StartPos']['x'] <= mouse[0] <= dict['StartPos']['x'] + dict['width'] and dict['StartPos']['y'] <= mouse[1] <= dict['StartPos']['y']+dict['height']: 
-        pygame.draw.rect(screen,color_light,[dict['StartPos']['x'],dict['StartPos']['y'],dict['width'],dict['height']]) # If mouse is hovering
+        pygame.draw.rect(screen,dict['hover_color'],[dict['StartPos']['x'],dict['StartPos']['y'],dict['width'],dict['height']]) # If mouse is hovering
         pygame.display.set_caption('Button')
     else: 
-        pygame.draw.rect(screen,color_dark,[dict['StartPos']['x'],dict['StartPos']['y'],dict['width'],dict['height']]) # If mouse is not touching
+        pygame.draw.rect(screen,dict['main_color'],[dict['StartPos']['x'],dict['StartPos']['y'],dict['width'],dict['height']]) # If mouse is not touching
         pygame.display.set_caption('Click on a button!')
     screen.blit(pygame.font.SysFont(dict['font'],dict['fontsize']).render(dict['text'] , True , white),(dict['StartPos']['x']+0,dict['StartPos']['y'])) # Putting text on the button
 
@@ -89,6 +84,7 @@ while running: # Main Loop (break after quit)
         if ev.type == pygame.QUIT: 
             pygame.quit() 
             running = False
+            break
               
         if ev.type == pygame.MOUSEBUTTONDOWN: # Mouse Click Checking
             # This is where you find out where the button was clicking then run something depending on the button
@@ -99,22 +95,32 @@ while running: # Main Loop (break after quit)
                 break
 
             if reaction_button['StartPos']['x'] <= mouse[0] <= reaction_button['StartPos']['x']+reaction_button['width'] and reaction_button['StartPos']['y'] <= mouse[1] <= reaction_button['StartPos']['y']+reaction_button['height']: 
-                
-                while True:
+                reaction_running = True
+                while reaction_running:
 
                     screen.fill((40,40,40))
+
                     screen.blit(bigfont.render('Reaction Game' , True , white),(100,60))
-                    
-                    clock.tick(60) # Capping at 60fps so my PC doesnt die
+                    screen.blit(smallfont.render('Press the button once it turns green!' , True , white),(80,580))
+
+                    pygame.draw.circle(screen, (255,0,0), (360,360) , 200)
+
 
                     for ev in pygame.event.get():
+
                         if ev.type == pygame.QUIT: 
-                            pygame.quit() 
+                            reaction_running = False
                             running = False
-                    if quit == 1:
+                            pygame.quit() 
+                            
+
+                    if reaction_running == False:
                         break   
 
+                    clock.tick(60) # Capping at 60fps so my PC doesnt die
                     pygame.display.update() # updates the frames of the game (always use)
+
+
 
     
     if running == False:
