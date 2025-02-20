b292f81dba8ddbcc609ba389fd26061796732d85
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-02-20 13:44:07 -0700

    messed around

--- high_score_tracker/main.py
+++ high_score_tracker/main.py
@@ -1,3 +1,35 @@
 # Alec George, Jackson Hauley, and Tate Morgan, High Score Tracker
 
+import pygame
+import random
 
+
+
+def main():
+    width = 720
+    height = 720
+    keep_running = True
+    while keep_running:
+        pygame.display.set_mode((width,height))
+        pygame.display.set_caption("e")
+
+        
+
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+            
+                width -= 20
+                height -= 20
+
+                if width <= 40:
+                    pygame.quit()
+                    keep_running = False
+
+        if keep_running:
+            pygame.display.update()
+    
+
+    
+
+
+main()
