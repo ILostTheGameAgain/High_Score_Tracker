2fcf3465b06a3c9cc0665656424f88fe616ede45
Tate-masterofCHAOS <tate.morgan@ucas-edu.net>
2025-02-20 13:44:29 -0700

    wokred on learning pygame

--- high_score_tracker/main.py
+++ high_score_tracker/main.py
@@ -1,3 +1,27 @@
 # Alec George, Jackson Hauley, and Tate Morgan, High Score Tracker
 
+import pygame
 
+
+
+def main():
+    width = 720
+    height = 720
+    run = True
+    while run:
+        pygame.display.set_mode((width,height))
+        pygame.display.set_caption("DELTARUNE")
+
+
+
+        for ev in pygame.event.get():
+            if ev.type == pygame.QUIT:
+
+                width -= 1
+                height -= 1
+                if width == 1 or height == 1:
+                    pygame.quit()
+                    run = False
+        if run:
+            pygame.display.update()
+main()
