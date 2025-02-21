46b99c30ab3a0897307493feafa80f753dbad0f4
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-02-21 13:44:13 -0700

    messed around more

--- high_score_tracker/main.py
+++ high_score_tracker/main.py
@@ -19,15 +19,18 @@ def main():
         for ev in pygame.event.get():
             if ev.type == pygame.QUIT:
                 color = 0
+                change = 5
                 r_offset = random.randint(0,255)
                 g_offset = random.randint(0,255)
                 b_offset = random.randint(0,255)
                 for i in range(255):
-                    width = random.randint(100,1000)
-                    height = random.randint(100,1000)
+                    width += change
+                    height += change
                     screen = pygame.display.set_mode((width,height)).fill((abs((r_offset-color))%255,abs((g_offset-color))%255,abs((b_offset-color))%255))
                     pygame.display.update()
                     color += 1
+                    if random.randint(1,10)==10:
+                        change *= -1
                     time.sleep(0.001)
                 clicks += 1
 
