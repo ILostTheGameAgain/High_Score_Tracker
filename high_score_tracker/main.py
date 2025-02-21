8fb37219414c7c9049f6b088d99a149e670c6a77
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-02-21 13:39:09 -0700

    messed around

--- high_score_tracker/main.py
+++ high_score_tracker/main.py
@@ -2,12 +2,14 @@
 
 import pygame
 import random
+import time
 
 
 
 def main():
     width = 720
     height = 720
+    clicks = 0
     keep_running = True
     while keep_running:
         pygame.display.set_mode((width,height))
@@ -17,11 +19,20 @@ def main():
 
         for ev in pygame.event.get():
             if ev.type == pygame.QUIT:
-            
-                width -= 20
-                height -= 20
+                color = 0
+                r_offset = random.randint(0,255)
+                g_offset = random.randint(0,255)
+                b_offset = random.randint(0,255)
+                for i in range(255):
+                    width = random.randint(100,1000)
+                    height = random.randint(100,1000)
+                    screen = pygame.display.set_mode((width,height)).fill(((r_offset-color)%255,(g_offset-color)%255,(b_offset-color)%255))
+                    pygame.display.update()
+                    color += 1
+                    time.sleep(0.001)
+                clicks += 1
 
-                if width <= 40:
+                if clicks == 5:
                     pygame.quit()
                     keep_running = False
 
