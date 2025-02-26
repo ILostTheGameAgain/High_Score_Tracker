2520d0c2a093e4f7374ce5cd209e6d08f2f09068
Jackson <jackson.hauley@ucas-edu.net>
2025-02-26 13:40:19 -0700

    rrrr

--- high_score_tracker/main.py
+++ high_score_tracker/main.py
@@ -16,6 +16,19 @@ quit_button = {
 "text_offset": 35,
 "text_color": (255,255,255)
 }
+reaction_button = {
+"width" : 150, # width of the button
+"height" : 40, # height of the button
+"StartPos": {"x" : 350 ,"y" : 650}, # Top left is 0,0
+"text": "Reaction",
+"font": "Arial",
+"fontsize": 35,
+"hover_color": (170,170,170),
+"main_color": (100,100,100) ,
+"text_offset": 5,
+"text_color": (255,255,255)
+}
+
 
 pygame.display.set_mode((720,720))
 
@@ -37,6 +50,9 @@ def main():
                 if quit_button['StartPos']['x'] <= mouse[0] <= quit_button['StartPos']['x']+quit_button['width'] and quit_button['StartPos']['y'] <= mouse[1] <= quit_button['StartPos']['y']+quit_button['height']: # Checking in this range for clicking the button
                     pygame.quit() 
                     running = False
+                    break
+                if reaction_button['StartPos']['x'] <= mouse[0] <= reaction_button['StartPos']['x']+reaction_button['width'] and reaction_button['StartPos']['y'] <= mouse[1] <= reaction_button['StartPos']['y']+reaction_button['height']: # Checking in this range for clicking the button
+                    reaction_game() 
                     break
         
         if run:
