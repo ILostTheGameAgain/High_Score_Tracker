6a7b3387593aeabf76f1d06b112bd50480406643
ILostTheGameAgain <alec.george@ucas-edu.net>
2025-02-28 13:24:35 -0700

    finishing touches on memory game, need to fix a bug

--- high_score_tracker/memory_game.py
+++ high_score_tracker/memory_game.py
@@ -10,38 +10,59 @@ import time
 from file_functions import *
 
 
-#function to add a random character to answer
-def add_letter(string):
+#function for normal mode, capitalization doesn't matter and it just adds a letter to the end
+def normal(string):
     #variable for all possible characters to add
-    possible_figures = "1234567890QWERTYIOPASDFGHJKLZXCVBNM"
+    possible_figures = "1234567890qwertyuiopasdfghjklzxcvbnm"
     #add a random letter
     string += possible_figures [random.randint(0,len(possible_figures)-1)]
     return string
 
 
+#function for hard mode, capitalization matters, more characters, and randomizes the string
+def hard(string, length):
+    possible_figures = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()_+-=~`[];:',<.>/?"
+    for i in range(length):
+        string += possible_figures[random.randint(0,len(possible_figures)-1)]
+
+    return string
+
+    
+
 #main function
 def memory():
     #introduction to the game
-    print("\nWelcome to the memory game!\nA string of letters/numbers will appear for about 3 seconds.\nYour goal is to type the string of letters again after it disappears.\nAs you get more and more correct, the string will get longer and longer.\npress enter to start")
-    input()
+    print("\nWelcome to the memory game!\nA string of letters/numbers will appear for about 3 seconds.\nYour goal is to type the string of letters again after it disappears.\nAs you get more and more correct, the string will get longer and longer.")
+    playing = True
     #allow the user to play again using a while loop
-    while True:
+    while playing:
+        mode = input("type 1 for normal mode\ntype 2 for hard mode\nyour answer here: ")
         #make variables for score and the string to guess
         score = 0
         answer = ""
         playing = True
         #while loop to run until the user fails
         while playing:
-            #add one character to answer
-            answer = add_letter(answer)
+            if mode == "1":
+                #add one character to answer if answer is 1 (normal mode)
+                answer = normal(answer)
+
+            elif mode == "2":
+                #randomize the string and add characters if answer is 2 (hard mode)
+                answer = hard(answer, score+1)
+
+            else: #exception handling
+                print("\ninvalid input")
+                continue
+
             #show the answer for 5 seconds
             print(f"\nstring:\n{answer}")
             time.sleep(3)
             print("\033c")
 
             #variable for the user's guess to what the string is
-            guess = input("what was the string? ").strip().lower()
-            if guess == answer.lower():
+            guess = input("what was the string? ").strip()
+            if guess == answer:
                 #if guess and answer are the same, increase score by 1 and add a letter
                 score += 1
                 print(f"\ncorrect!\nscore: {score}")
@@ -69,3 +90,4 @@ def memory():
                 #exception handling
                 print("\ninvalid input")
 
+memory()
